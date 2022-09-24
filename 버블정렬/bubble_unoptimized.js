cf42f84782dbf733667d13421f9b3e2dccce584c
jsleemaster <roeowo123@naver.com>
2022-09-24 13:21:55 +0900

    버블정렬

---
+++ 버블정렬/bubble_unoptimized.js
@@ -0,0 +1,30 @@
+function bubbleSort(arr) {
+  for (var i = arr.length; i > 0; i--) { // 끝 배열을 생략하기 위함
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);

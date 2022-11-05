d35ed1e5f9736379d678cf37c1c546659eeb7022
jsleemaster <roeowo123@naver.com>
2022-11-05 13:58:40 +0900

    reserve

--- 자료구조/단일연결.js
+++ 자료구조/단일연결.js
@@ -104,7 +104,20 @@ class SinglyLinkedList {
         previousNode.next = removed.next;
         this.length--;
         return removed;
-
+    }
+    reserve() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
     }
 }
 

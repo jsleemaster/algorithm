23841fd97dd86b62084f1bff84a9cd6b12bce064
jsleemaster <roeowo123@naver.com>
2022-11-19 13:46:31 +0900

    이중 연결

--- 자료구조/이중연결.js
+++ 자료구조/이중연결.js
@@ -11,4 +11,16 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val)
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+    this.length++;
+    return this;
+  }
 }

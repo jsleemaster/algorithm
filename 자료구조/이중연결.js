70d71d652014622069e45099141d9774878d2cdd
jsleemaster <roeowo123@naver.com>
2022-11-20 10:45:15 +0900

    이중연결 pop

--- 자료구조/이중연결.js
+++ 자료구조/이중연결.js
@@ -23,4 +23,18 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined
+    let popNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = popNode.prev;
+      this.tail.next = null;
+      popNode.prev = null;
+    }
+    this.length--;
+    return popNode;
+  }
 }

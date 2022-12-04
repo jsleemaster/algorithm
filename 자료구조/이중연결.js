81202f01be6907f7f8b82fdeee5449b8c7f16ff6
jsleemaster <roeowo123@naver.com>
2022-12-04 22:20:04 +0900

    set method

--- 자료구조/이중연결.js
+++ 자료구조/이중연결.js
@@ -84,4 +84,12 @@ class DoublyLinkedList {
     }
     return current
   }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }

2702a9e25824a7d8f474125474611ca4587dfa00
jsleemaster <roeowo123@naver.com>
2023-01-21 16:25:57 +0900

    extractMax

--- 자료구조/힙.js
+++ 자료구조/힙.js
@@ -31,8 +31,48 @@ class MaxBinaryHeap {
   }
   extractMax() { // 삭제
     //1. 자리교체
-    const max = this.values[0]
+    const max = this.values[0];
     const end = this.values.pop();
-    this.values[0] = end
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
   }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[idx];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let swap = null;
+      let leftChild, rightChild;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild < element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+
+      this.values[idx] = this.values[swap]; //자리 바꾸기
+      this.values[swap] = element;
+      idx = swap;
+    }
+
+  }
+
 }

3b99ba79b3072c908d5a76be00f1eeed359573d2
jsleemaster <roeowo123@naver.com>
2023-01-26 20:07:59 +0900

    addVertext, addEdge 방향 그래프 추가

--- 자료구조/그래프.js
+++ 자료구조/그래프.js
@@ -19,3 +19,20 @@
  * 비가중 그래프
  * 간선에 값이 부여되지 않은 경우
  */
+
+//그래프 정렬 방법 - 인접 행렬 (공간이 넓음, 특정 간선을 확인할때 효율적), *인접 리스트(공간이 적음, 모든 간선을 순회하는 것은 빠름 더 중요 -실제로 많이 이용됨 )
+//정렬 : 정점과 간선과의 관계를 구하는 법
+
+//무방향 그래프
+class Graph {
+  constructor() {
+    this.adjacenyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacenyList[vertex]) this.adjacenyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacenyList[vertex1].push(vertex2)
+    this.adjacenyList[vertex2].push(vertex1)
+  }
+}

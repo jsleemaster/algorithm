ec2352cd41980699b1b5465ab1833e74263760be
jsleemaster <roeowo123@naver.com>
2023-02-26 23:33:37 +0900

    중복문제

--- 동적프로그래밍/중복되는문제.js
+++ 동적프로그래밍/중복되는문제.js
@@ -1,5 +1,9 @@
 //피보나치수열, 합병정렬이 이에 속한다.
-const fib = (number) => {
+const fib = (number, memo = []) => {
+  //중복되는 작업이 많기 때문에 메모이제이션을 추가한다.
+  if (memo[number] !== undefined) return memo[number];
   if (number <= 2) return 1;
-  return fib(number - 1) + fib(number - 2)
+  const value = fib(number - 1) + fib(number - 2)
+  memo[number] = value;
+  return memo[number];
 }

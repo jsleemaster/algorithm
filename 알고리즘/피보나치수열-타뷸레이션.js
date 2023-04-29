6f1b32379ea745858012835d1ed2b1d2768bee96
jsleemaster <roeowo123@naver.com>
2023-04-29 13:26:31 +0900

    피보나치 활용 문제

--- 알고리즘/피보나치수열-타뷸레이션.js
+++ 알고리즘/피보나치수열-타뷸레이션.js
@@ -2,9 +2,11 @@
 const fib = (number) => {
   //공간을 이용
   if (number <= 2) return 1;
-  let value = [0, 1, 1]
-  for (let i = 3; i < number; i++) {
+  let value = [0, 1, 2]
+  for (let i = 3; i <= number; i++) {
     value[i] = value[i - 1] + value[i - 2];
   }
   return value[number];
 }
+
+console.log(fib(4))

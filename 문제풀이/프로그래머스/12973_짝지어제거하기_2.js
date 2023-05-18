ab1e2561e2ae3908a0706ee817c8d0ed07812acb
jsleemaster <roeowo123@naver.com>
2023-05-18 20:05:50 +0900

    짝지어제거하기

---
+++ 문제풀이/프로그래머스/12973_짝지어제거하기_2.js
@@ -0,0 +1,16 @@
+
+function solution(s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === stack.at(-1)) {
+            stack.pop()
+        } else {
+            stack.push(s[i])
+        }
+    }
+    return stack.length ? 0 : 1
+}
+
+//1
+console.log(solution("baabaa"))

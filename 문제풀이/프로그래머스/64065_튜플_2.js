9034664503ac1128c1c14df5590865d9b13d6b75
jsleemaster <roeowo123@naver.com>
2023-06-01 23:19:44 +0900

    튜플

---
+++ 문제풀이/프로그래머스/64065_튜플_2.js
@@ -0,0 +1,14 @@
+
+function solution(s) {
+  const answer = [];
+  const arr = JSON.parse(s.replace(/{/g, "[").replace(/}/g, "]"));
+  arr.sort((a, b) => a.length - b.length);
+  arr.forEach((col) => {
+    col.forEach((row) => {
+      if (!answer.includes(row)) answer.push(row);
+    });
+  });
+  return answer;
+}
+
+console.log(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")) //[2,1,3,4]

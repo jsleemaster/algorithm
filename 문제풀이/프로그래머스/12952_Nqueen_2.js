918e8fab8fecdf6b288fdbdea8ee621a43869af3
jsleemaster <roeowo123@naver.com>
2023-06-27 20:22:50 +0900

    nqueen

--- 문제풀이/프로그래머스/12952_Nqueen_2.js
+++ 문제풀이/프로그래머스/12952_Nqueen_2.js
@@ -1,13 +1,12 @@
 function solution(n) {
 
-  // 배열의 값은 해당 행의 queen이 있는 '열(column)'을 의미함
   const board = Array(n).fill(0);
   let answer = 0;
 
   const valid = (i) => {
     for (let j = 0; j < i; j++) {
       if (board[i] === board[j]) return false;
-      if (Math.abs(i - j) == Math.abs(board[i] - board[j])) return false;
+      if (Math.abs(i - j) === Math.abs(board[i] - board[j])) return false;
     }
     return true;
   }
